f165af872684b6abc25149adb0eb2f5904c92294
rahkeemg <rahkeemg@live.com>
2020-07-19 12:47:53 -0400

    Updated template folder with navigator view and refactor views to house navigator as a main view for overrides

--- application/app.py
+++ application/app.py
@@ -8,11 +8,14 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
 
 
 if __name__ == '__main__':
